34dacc8dbd38b38bbecca962f11b914c2a42918d
James Perisher <jamesperiser@outlook.com>
2019-07-27 19:17:09 +1000

    prepared for forms

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import json, sqlite3
 import time, pytz
 import numpy as np
 import os
-import db_interact.py
+import db_interact
 
 app = Flask(__name__, template_folder='templates')
-app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route("/")
 def home():
