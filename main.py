d96a3bbede252180ccb5929ab300eb33d4dc8593
JKookaburra <JKookaburraSchool@gmail.com>
2019-07-27 18:46:50 +1000

    minor update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template, redirect
-import json, sqlite3
+import json
 import time, pytz
 import numpy as np
 import os
-
+import db_interact.py
 
 app = Flask(__name__, template_folder='templates')
 
