de796230e4c5883b8ca4cd67833da12669ef92b5
JKookaburra <JKookaburraSchool@gmail.com>
2019-07-27 19:21:45 +1000

    changes to db_interact and main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import render_template, redirect
 from flask import request
-import json, sqlite3
+import json
 import time, pytz
 import numpy as np
 import os
-import db_interact.py
+import db_interact as db
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'you-will-never-guess'
@@ -23,6 +23,7 @@ def add_person():
     print(request.args)
     return render_template("person_form.html")
 
+db.data_entry()
 
 if __name__ == '__main__':
     app.run()
