f922cbf00e4444571e2fe0db58f564df9f3dba66
James Perisher <37967469+JamesPerisher@users.noreply.github.com>
2019-07-28 12:47:53 +1000

    added class for forms

--- main.py
+++ main.py
@@ -11,6 +11,31 @@ import db_interact as db
 app = Flask(__name__, template_folder='templates')
 
 
+class form():
+    def call(self):
+        if self.request.args.to_dict() == {}:
+            return render_template(self.form, fields=self.empty)
+
+        base = self.empty.copy()
+        res_data = self.request.args.to_dict()
+        base.update(res_data)  # makse sure all keys in self.empty are included in base
+
+        if "" in [base[x] for x in base]:
+            return render_template(self.form, error=("All fields must be filled." if self.error == None else self.error), fields=base)
+        self.success = self.event(base) # calls event funtion
+        print("Data: %s"%base)
+        return render_template(self.form, success=("Success" if self.success == None else self.success), fields=self.empty)
+
+    def __init__(self, request, empty, form, event=lambda x: "data processed but no event has been added", error=None, success=None):
+        self.request = request
+        self.empty = empty
+        self.form = form
+        self.event = event
+        self.error = error
+        self.success = success
+
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -25,32 +50,14 @@ def home_redirect():
 def add_person():
     empty = {"name_first": "", "name_last": "", "gender": "", "year": "",
              "house": "", "dob": "", "teacher": "", "student_id": ""}
-    if request.args.to_dict() == {}:
-        return render_template("person_form.html", fields=empty)
-    base = empty.copy()
-    res_data = request.args.to_dict()
-    base.update(res_data)
-
-    if "" in [base[x] for x in base]:
-        return render_template("person_form.html", error="All fields must be filled.", fields=base)
-    return render_template("person_form.html", success="Successfully created user: %s %s" % (base["name_first"], base["name_last"]), fields=empty)
-    # TODO: call add to datbase funtion data is in base
+    f = form(request, empty, "person_form.html")
+    return f.call()
 
 @app.route('/add_event')
 def add_event():
     empty = {}
-    return render_template("event_form.html", fields=empty)
-    if request.args.to_dict() == {}:
-        return render_template("person_form.html", fields=empty)
-    return
-    base = empty.copy()
-    res_data = request.args.to_dict()
-    base.update(res_data)
-
-    if "" in [base[x] for x in base]:
-        return render_template("event_form.html", error="All fields must be filled.", fields=base)
-    return render_template("event_form.html", success="Successfully created event: %s"%(base["name"]), fields=empty)
-    # TODO: call add to datbase funtion data is in base
+    f = form(request, empty, "event_form.html")
+    return f.call()
 
 
 c = db.connection()
