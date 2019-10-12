ae5f747242be8dfcc3b54551f63b3d3b3806da70
James Perisher <37967469+JamesPerisher@users.noreply.github.com>
2019-10-12 14:24:30 +1100

    start db thread

--- db_interact.py
+++ db_interact.py
@@ -88,6 +88,7 @@ class connection():
 
         self.log = log.basicConfig(filename='db/%s/%s/%s-%s.log'%(datetime.date.today().year ,datetime.date.today().month, datetime.date.today(), os.path.basename(__file__)[:-3]), level=log.DEBUG, format='%(asctime)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
         self.c = DatabaseManager(database)
+        self.c.start()
 
         self.create_db()
 
