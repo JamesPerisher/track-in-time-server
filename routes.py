47ccc78d4b51ad916e33e47efa5e795d6880a20b
James Perisher <37967469+JamesPerisher@users.noreply.github.com>
2019-10-13 21:31:14 +1100

    better forms

--- routes.py
+++ routes.py
@@ -9,37 +9,14 @@ import os
 import db_interact as db
 import datetime
 
+from input_classes import *
+
 app = Flask(__name__, template_folder='templates')
 app.debug = True
 
 
 
 # log.basicConfig(filename='%s.log'%__name__, level=log.DEBUG, format='%(asctime)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-
-
-class form():
-    def call(self):
-        if self.request.args.to_dict() == {}:
-            return render_template(self.form, fields=self.empty)
-
-        base = self.empty.copy()
-        res_data = self.request.args.to_dict()
-        # makse sure all keys in self.empty are included in base
-        base.update(res_data)
-        print("Data: %s" % base)
-
-        if "" in [base[x] for x in base]:
-            return render_template(self.form, error=("All fields must be filled." if self.error == None else self.error), fields=base)
-        self.success = self.event(base)  # calls event funtion
-        return render_template(self.form, success=("Success" if self.success == None else self.success), fields=self.empty)
-
-    def __init__(self, request, empty, form, event=lambda x: "data processed but no event has been added", error=None, success=None):
-        self.request = request
-        self.empty = empty
-        self.form = form
-        self.event = event
-        self.error = error
-        self.success = success
 
 
 @app.route("/submitform", methods=['POST'])
@@ -59,25 +36,51 @@ def home_redirect():
     return redirect("/", code=302)
 
 
-@app.route('/add_student')
+@app.route('/add_student', methods = ["GET","POST"])
 def add_student():
-    return render_template("add_student_form.html", error = None, success=None)
+    if request.method == "GET":
+        elements = [
+        input_text("First Name", "name_first"),
+        input_text("Last Name", "name_last"),
+        input_gender("Gender", "gender"),
+        input_yearGroup("Year", "age_group"),
+        input_house("Element", "house"),
+        input_dob("Date of birth", "dob"),
+        input_text("Student id", "stu_id"),
+        input_submit("Submit")
+        ]
+        return render_template("input_template.html", elements=elements)
 
+    if request.method == "POST":
+        if check_data(request)[0]:
+            # success
+            # TODO: call db create function
+            return redirect(url_for('.add_student', success="Success passing data."))
+        else:
+            return redirect(url_for('.add_student', error="All fields are required"))
 
-@app.route('/add_teacher')
-def add_teacher():
-    empty = {"name_first": "", "name_last": "", "gender": "", "year_groups": "",
-             "house": "", "dob": ""}
-    f = form(request, empty, "add_teacher_form.html")
-    return f.call()
+#
+#     empty = {"age_group": "",
+#              "track_feild": "", "timed_score_distance": ""}
+# @app.route('/add_event', methods = ["GET","POST"])
+# def add_event():
+#     if request.method == "GET":
+#         elements = [
+#         input_text("Event Name", "name"),
+#         input_gender("Gender", "gender"),
+#         input_submit("Submit")
+#         ]
+#         return render_template("add_event.html", elements=elements)
+#
+#     if request.method == "POST":
+#         if check_data(request)[0]:
+#             # success
+#             # TODO: call db create function
+#             return redirect(url_for('.add_event', success="Success passing data."))
+#         else:
+#             return redirect(url_for('.add_event', error="All fields are required"))
+#
 
-
-@app.route('/add_event')
-def add_event():
-    empty = {"name": "", "time": "", "age_group": "",
-             "track_feild": "", "timed_score_distance": "", "gender": ""}
-    f = form(request, empty, "add_event_form.html", event=lambda x: x)
-    return f.call()
 
 
 @app.route('/cmd')
