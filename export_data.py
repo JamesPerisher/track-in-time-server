3784a0070926bf81112026fa902b2c1085e5d0fd
JKookaburra <JKookaburraSchool@gmail.com>
2019-11-28 04:58:37 +1100

    mass update

--- export_data.py
+++ export_data.py
@@ -19,7 +19,9 @@ import db_interact as db
 import pandas as pd
 import numpy as np
 import os
+import time
 
+from datetime import date
 
 
 class data():
@@ -37,6 +39,7 @@ class data():
         writer = pd.ExcelWriter('downloads/points.xlsx', engine='xlsxwriter')
 
         test_list = [5,4,2]
+        results_list = []
 
         events = self.db.get_events()
         if events == []:
@@ -45,58 +48,62 @@ class data():
             for count,i in enumerate(events):
                 results = {}
                 results["Students"] = []
-                print(i)
-                results["%s - %s"% (i[3], i[5])] = []
+                results["House"] = []
+                results["Score"] = []
+                results["Points"] = []
+                results["ID"] = []
                 event_results = self.db.get_results_from_event(i[0])
-                print(event_results)
-                print(event_results)
-                for a,b in zip(test_list, event_results):
-                    results["%s - %s"% (i[3], i[5])].append(a)
-                    student = (self.db.get_participant_info(b[0], "db_id"))[0]
+
+                for amount in test_list[0:len(event_results)]:
+                    results["Points"].append(amount)
+
+                for _ in event_results[len(test_list)::]:
+                    results["Points"].append(1)
+
+                for result in event_results:
+                    student = (self.db.get_participant_info(result[1], "db_id"))[0]
+                    results["ID"].append(student[0])
+                    results["House"].append(student[5])
                     results["Students"].append("%s %s"%(student[2], student[1]))
-                    # print(self.db.get_participant_info(b[0], "db_id"), a)
-                for j in event_results[len(test_list)::]:
-                    results["%s - %s"% (i[3], i[5])].append(1)
-                    student = (self.db.get_participant_info(j[0], "db_id"))[0]
-                    results["Students"].append("%s %s"%(student[2], student[1]))
-                    # print(self.db.get_participant_info(j[0], "db_id"), str(1))
-                # print()
+                    results["Score"].append(result[3])
 
                 # print(results)
+                results_list.append(results)
+
+                info = pd.DataFrame({1:[i[2]],2:[i[3]],3:[i[5]]})
+                results.pop("ID")
                 all_data = pd.DataFrame(results)
-                all_data.to_excel(writer, sheet_name="points", index=False, header=True, startcol=count*3)
+                info.to_excel(writer, sheet_name="points", index=False, header=False, startcol=count*5, startrow=0)
+                all_data.to_excel(writer, sheet_name="points", index=False, header=True, startcol=count*5, startrow=2)
+
+
+        print(self.point_adder(results_list))
+
 
         writer.save()
 
 
+# {dwdwd:[efd,fe,sdg,ef], wad:[gr,awd,wad,aw]}
+
     def excel_all(self):
         # print(self.db.get_events())
 
-        writer = pd.ExcelWriter('downloads/all.xlsx', engine='xlsxwriter')
+        name = "EventResults_%s-%s.xlsx" %(date.today().strftime("%d.%m.%y"),str(time.time()).split(".")[0].strip())
+
+        writer = pd.ExcelWriter('downloads/%s'%name, engine='xlsxwriter')
 
         events = self.db.get_events()
+        results = self.db.get_results()
+
+        data_to_add = {}
+        for i in results:
+            try:
+                data_to_add[i[1]].append(i)
+            except KeyError:
+                data_to_add[i[1]] = [i]
 
 
-        data_to_add = {"Student":[],"Score":[]}
-        for i in events:
-
-            data = self.db.get_results_from_event(i[0])
-            event =  self.db.get_event_info(data[0][2], "id")[0]
-            print(event[2])
-
-            for j in data:
-                student = self.db.get_participant_info(j[1], "db_id")[0]
-
-                data_to_add["Student"].append(student[0])
-                data_to_add["Score"].append(j)
-                print(student[2], student[1], j[3])
-
-
-        print(data_to_add)
-        print("done")
-        {'Student': ['Oli Dicer', 'Dana Atkins'], 'Score': [24.0, 1234.0]}
         # data_to_add = {198: [(3, 198, 1, 42.0)], 202: [(1, 202, 1, 356.0)], 208: [(2, 208, 1, 43.0)], 209: [(4, 209, 1, '`12')], 212: [(5, 212, 1, 43.0)], 275: [(7, 275, 2, 32.0), (10, 275, 3, 4356.0), (12, 275, 4, 1234.0)], 276: [(11, 276, 3, 12.0), (13, 276, 4, 24.0)], 277: [(9, 277, 2, 24.0)], 282: [(6, 282, 2, 134.0)], 283: [(8, 283, 2, '`1')]}
-
 
         event_ids = [x[0] for x in events]
 
@@ -136,7 +143,6 @@ class data():
 
 
         for event in events:
-            print(event)
             final["%s - %s"% (event[2], event[0])] = []
 
         for i in o:
@@ -149,19 +155,26 @@ class data():
                 final["%s - %s"% (event[2], event[0])].append(val)
                 # final[j].append([])
         # print(o)
-        print(final)
 
 
         all_data = pd.DataFrame(final)
-        print(event)
         event_data = pd.DataFrame({"should_not_be_seeing_this":event[2::]})
         event_data.to_excel(writer, sheet_name=event[2], index=False, header=False, startrow=0)
         all_data.to_excel(writer, sheet_name=event[2], index=False, startcol=2)
 
         writer.save()
 
+    def point_adder(self, b):
+        out = {}
 
+        for a in b:
+            for i in range(len(a["Points"])):
+                try:
+                    out[a["Students"][i]] += a["Points"][i]
+                except KeyError:
+                    out[a["Students"][i]] = a["Points"][i]
 
+        return out
 
     def excel_winners(self):
         try:
@@ -172,6 +185,8 @@ class data():
 
         # exel_winners = pd.read_excel("/random.xlsx")
 
+
+
 if __name__ == '__main__':
     db = db.connection()
     db.start(5)
@@ -179,7 +194,7 @@ if __name__ == '__main__':
 
     data = data(db)
     data.excel_winners()
-    data.excel_all()
-    # data.get_champs()
+    # data.excel_all()
+    data.get_champs()
     db.kill()
     exit()
